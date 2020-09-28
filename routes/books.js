7f2ec30382412952134e576d28cb2315b82d18ca
Potito Gradone <pgradone@gmail.com>
2020-09-28 10:11:20 +0200

    remove commented code

--- routes/books.js
+++ routes/books.js
@@ -37,29 +37,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-// All Books Route final
-// router.get('/', async (req, res) => {
-//   let query = Book.find();
-//   if (req.query.title != null && req.query.title != '') {
-//     query = query.regex('title', new RegExp(req.query.title, 'i'));
-//   }
-//   if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
-//     query = query.lte('publishDate', req.query.publishedBefore);
-//   }
-//   if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
-//     query = query.gte('publishDate', req.query.publishedAfter);
-//   }
-//   try {
-//     const books = await query.exec();
-//     res.render('books/index', {
-//       books: books,
-//       searchOptions: req.query,
-//     });
-//   } catch {
-//     res.redirect('/');
-//   }
-// });
-
 // New Book Route
 router.get('/new', async (req, res) => {
   renderNewPage(res, new Book());
